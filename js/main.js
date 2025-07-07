0e34c00efdc7cf8b1c8d0863fe085f3a9aaa0cb8
AntonioGoncalvesSousa <antonio.sousa@sptech.school>
2025-07-07 13:14:02 -0300

    Adicionando o js

--- js/main.js
+++ js/main.js
@@ -42,3 +42,128 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 modalCloseBtn.addEventListener('click', testimonialsModalFunc);
 overlay.addEventListener('click', testimonialsModalFunc);
 
+//Activating Modal-projects
+
+const projectsItem = document.querySelectorAll('[data-filter-item]');
+const modalProjectsContainer = document.querySelector('[data-modal-container-projects]');
+const modalProjectsCloseBtn = document.querySelector('[data-modal-close-btn-projects]');
+const overlayProjects = document.querySelector('[data-overlay-projects]');
+
+const modalProjectsImg = document.querySelector('[data-modal-projects-img]');
+const modalProjectsTitle = document.querySelector('[data-modal-projects-title]');
+const modalProjectsText = document.querySelector('[data-modal-projects-text]');
+const modalProjectsLink = document.querySelector('[data-modal-projects-link]');
+
+// Function to toggle modal-projects
+
+const projectsModalFunc = function () {
+    modalProjectsContainer.classList.toggle('active');
+    overlayProjects.classList.toggle('active');
+}
+
+//Activating modal-projects
+for (let i = 0; i < projectsItem.length; i++) {
+    projectsItem[i].addEventListener('click', function () {
+        modalProjectsImg.src = this.querySelector('[data-projects-img]').src;
+        modalProjectsImg.alt = this.querySelector('[data-projects-img]').alt;
+        modalProjectsTitle.innerHTML = this.querySelector('[data-projects-title]').innerHTML;
+        modalProjectsText.innerHTML = this.querySelector('[data-projects-desc]').innerHTML;
+        modalProjectsLink.href = this.querySelector('[data-projects-link]').href;
+
+        projectsModalFunc();
+    })
+}
+
+//Activating close button in modal-projects
+modalProjectsCloseBtn.addEventListener('click', projectsModalFunc);
+overlayProjects.addEventListener('click', projectsModalFunc);
+
+//Activating Filter Select and filtering options
+
+const select = document.querySelector('[data-select]');
+const selectItems = document.querySelectorAll('[data-select-item]');
+const selectValue = document.querySelector('[data-select-value]');
+const filterBtn = document.querySelectorAll('[data-filter-btn]');
+
+select.addEventListener('click', function () { elementToggleFunc(this); });
+
+for (let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener('click', function () {
+
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        elementToggleFunc(select);
+        filterFunc(selectedValue);
+
+    });
+}
+
+const filterItems = document.querySelectorAll('[data-filter-item]');
+
+const filterFunc = function (selectedValue) {
+    for (let i = 0; i < filterItems.length; i++) {
+        if (selectedValue == "all") {
+            filterItems[i].classList.add('active');
+        } else if (selectedValue == filterItems[i].dataset.category) {
+            filterItems[i].classList.add('active');
+        } else {
+            filterItems[i].classList.remove('active');
+        }
+    }
+}
+
+//Enabling filter button for larger screens 
+
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+    filterBtn[i].addEventListener('click', function () {
+
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        filterFunc(selectedValue);
+
+        lastClickedBtn.classList.remove('active');
+        this.classList.add('active');
+        lastClickedBtn = this;
+
+    })
+}
+
+// Enabling Contact Form
+
+const form = document.querySelector('[data-form]');
+const formInputs = document.querySelectorAll('[data-form-input]');
+const formBtn = document.querySelector('[data-form-btn]');
+
+for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener('input', function () {
+        if (form.checkValidity()) {
+            formBtn.removeAttribute('disabled');
+        } else {
+            formBtn.setAttribute('disabled', '');
+        }
+    })
+}
+
+// Enabling Page Navigation 
+
+const navigationLinks = document.querySelectorAll('[data-nav-link]');
+const pages = document.querySelectorAll('[data-page]');
+
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function () {
+
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() == pages[i].dataset.page) {
+                pages[i].classList.add('active');
+                navigationLinks[i].classList.add('active');
+                window.scrollTo(0, 0);
+            } else {
+                pages[i].classList.remove('active');
+                navigationLinks[i].classList.remove('active');
+            }
+        }
+    });
+}
